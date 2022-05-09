434a3054935581f205cc54733997b70a2728fd88
lfs <1848029085@qq.com>
2022-05-09 19:52:30 +0800

    实现上传图片功能

--- lfs-framework/src/main/java/com/lfs/service/impl/UserServiceImpl.java
+++ lfs-framework/src/main/java/com/lfs/service/impl/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.lfs.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lfs.domain.ResponseResult;
 import com.lfs.domain.entity.User;
+import com.lfs.domain.vo.UserInfoVo;
 import com.lfs.mapper.UserMapper;
 import com.lfs.service.UserService;
+import com.lfs.utils.BeanCopyUtils;
+import com.lfs.utils.SecurityUtils;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,5 +19,16 @@ import org.springframework.stereotype.Service;
 @Service("userService")
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
+    //    个人信息
+    @Override
+    public ResponseResult userInfo() {
+//        获取当前的用户ID
+        Long userId = SecurityUtils.getUserId();
+//        根据用户ID查询用户信息
+        User user = getById(userId);
+//        封装成UserInfoVo
+        UserInfoVo vo = BeanCopyUtils.copyBean(user, UserInfoVo.class);
+        return ResponseResult.okResult(vo);
+    }
 }
 
